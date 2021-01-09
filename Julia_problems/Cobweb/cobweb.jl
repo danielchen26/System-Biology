825b02b929ee4f763a1fe4db7a1d1838912acd75
Danielchen26 <chentianchi@gmail.com>
2021-01-09 13:57:19 -0500

    change folder and 3 example plots

--- Julia_problems/Cobweb/cobweb.jl
+++ Julia_problems/Cobweb/cobweb.jl
@@ -1,7 +1,7 @@
 using Plots;gr()
-
-f(x) = 0.8*x.*(2-x)
-
+r = 0.7
+f(x) = r*x.*(3-x)
+f(x) = x*(1 + 2.5(1-x))
 function check_intercept()
     plot(0:0.1:2, f.(0:0.1:2))
     plot!(x->x,0:2)
@@ -10,8 +10,8 @@ check_intercept()
 
 
 function cobweb(f, a, b, N, x0)
-    @show interval = range(a, b,length = 100) # or LinRange(a,b,N)
-    plot(interval,f.(interval))
+    rr = range(a, b,length = 100) # or LinRange(a,b,N)
+    plot(rr,f.(rr))
     # Plot orbit starting at x0
     plt1 = scatter!([x0],[x0],c=:red, markerstrokecolor=:red)
     x = zeros(N);x[1] = x0; [x[i+1]=f(x[i]) for i in 1:N-1];
@@ -22,14 +22,17 @@ function cobweb(f, a, b, N, x0)
     end 
     # xlims!(a,b)
     # ylims!(a,b)
-    plot!(x -> x, 0, x[end])
-    display(plt1)
+    plot!(x -> x, 0, 2)
+    title!("Cobweb")
+    return x, display(plt1)
 end
 
-
-a = 0;b=2;N =5;x0=0.5
-cobweb(f, a, b, N, x0)
-
-
-
+##
+a = 0
+b=2
+N =30
+x0=0.05
+x, _ = cobweb(f, a, b, N, x0)
+plot(x)
+##
 
